28a29537c8249c4a3527532a7e00aaff9b4ed6cc
tie303053 <tie303053@S1711C12618-T1.tisnt.tis.co.jp>
2018-06-19 16:02:01 +0900

    test

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,30 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
+		int m = 0; // 文字列変換対象数値
+		StringBuilder result = new StringBuilder(); // 結果格納文字列
+
+		m = Integer.parseInt(args[args.length]);
+
+		for (int i = 0, l = args.length - 1; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
 			System.out.println(output);
+			result = result.append(fizzbuzz(m, args[i]));
 		}
+
+		// どれも割り切れなければmを返す
+		if(result.length() == 0) {
+			System.out.println(String.valueOf(m));
+		} else {
+			System.out.println(result.toString());
+		}
+	}
+
+	static String fizzbuzz(int num, String input) {
+		String[] split = input.split(":");
+		int cut = Integer.parseInt(split[0]);
+		if (num % cut == 0)
+			return split[1];
+		return "";
 	}
 }
